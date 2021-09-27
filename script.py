0a2f18c911b906fbe59cf332e2eec374905d30af
Niranjanmt6 <88812743+Niranjanmt6@users.noreply.github.com>
2021-09-27 07:19:23 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,50 +2,15 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
+ഹായ് ഞാൻ Filmclub ഗ്രൂപ്പിന് വേണ്ടി വർക്ക്‌ ചെയുന്ന ഒരു അൺലിമിറ്റഡ് ഫിൽറ്റർ ബോട്ട് ആണ്
+എന്നെ നിങ്ങൾക്ക് മറ്റൊരു ഗ്രൂപ്പിലും ഉപയോഗിക്കാൻ പറ്റില്ല :)
 
 See <i>/help</i> for commands and more details.</b>
 """
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
-
-
-<b>Basic Commands;</b>
-
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
-
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+നിങ്ങൾക്ക് ഞാൻ ഒരു ഹെല്പും ചെയ്യില്ല.😏
 """
 
 
